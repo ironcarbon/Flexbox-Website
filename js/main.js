b0c678e397b89734b59abb17b491cbe816285c7e
ipek <ipekcivicakan@gmail.com>
2019-03-06 13:51:00 -0800

    Smooth Scrolling & Sticky Menu Background Changed

--- js/main.js
+++ js/main.js
@@ -10,3 +10,46 @@ function initMap() {
     // The marker, positioned at location
     const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+
+//Sticky Menu Background
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+})
+
+
+
+
+//Smooth Scrolling
+
+$("#navbar a, .btn").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        const hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 65
+            },
+            800,
+
+            // function () {
+
+            //     // Add hash (#) to URL when done scrolling (default click behavior)
+            //     window.location.hash = hash;
+            // }
+        );
+    } // End if
+});
